1322ffb12af77c4fee91fc879e4a171996492d8e
Shaojing Chen <shaojing.chen@ericsson.com>
2018-04-13 16:30:37 +0800

    finish Ch11

--- PowerShell_CookBook_PartIII_Chapter11.ps1
+++ PowerShell_CookBook_PartIII_Chapter11.ps1
@@ -98,3 +98,122 @@ function Demo-Param{
         [string[]] $Name
     )
 }
+#11.13 Accept Script Block Parameters with Local Variables
+<#
+.SYNOPSIS
+Demonstrates the GetNewClosure() method on a scirpt block that pulls variable in from the user's session.
+
+.EXAMPLE
+PS>$name="Hello There"
+PS>Invoke-ScriptBlockClosure {$name}
+Hello There
+Hello World
+Hello There
+
+#>
+param(
+    ##The Script block to invoke
+    [scriptblock] $ScriptBlock
+)
+Set-StrictMode -Version 3
+## Create a new script block that pulls variables
+## from the user's scope
+$closedScriptBlock = $ScriptBlock.GetNewClosure()
+
+## Invoke the script block normally. The contents of 
+## the $name variable will be from the user's session.
+& $ScriptBlock
+
+## Define a new variable
+$name = "Hello World"
+
+## Invoke the script block normaly. the contents of 
+## the $name variable will be "Hello World", now from
+## our scope.
+& $ScriptBlock
+
+## Invoke the "closed" script block. The contents of
+## the $name variable will still be whatever was in the user's session
+& $closedScriptBlock
+
+#11.14 Dynamically Compose Command Parameters
+$1114par = @{
+        Name = "PowerShell";
+        Whatif = $true
+}
+Stop-Process @1114par
+
+#11.15 Provide -WhatIf, -Confirm, and Other Cmdlet Features
+function Invoke-MyAdvancedFunction
+{
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param()
+    if($PSCmdlet.ShouldProcess("test.txt","Remove Item"))
+    {
+        "Removing test.txt"
+    }
+    Write-Verbose "Verbose Message"
+}
+function Invoke-MyDangerousFunction
+{
+    [CmdletBinding()]
+    param(
+        [Switch] $Force
+    )
+    if($Force -or $PSCmdlet.ShouldContinue(
+        "Do you with to invoke this dangerous operation?
+        Changes can not be undone.","Invoke dangerous action?"))
+    {
+        "Invoking dangerous action"
+    }
+
+}
+#11.16 Add Help to Scripts or Functions
+
+#11.17 Add Custom Tags to a Function or Script Block
+
+#11.18 Access Pipeline Input
+function InputCounter
+{
+    $count = 0
+    ##############################
+    #.SYNOPSIS
+    # Go through each element in the pipeline, and add up
+    # how many elements there were.
+    #.DESCRIPTION
+    #Long description
+    #
+    #.EXAMPLE
+    #An example
+    #
+    #.NOTES
+    #General notes
+    ##############################
+    foreach($element in $input)
+    {
+        $count++
+    }
+    $count    
+}
+function ReverseInput
+{
+    $inputArray = @($input)
+    $inputEnd = $inputArray.Count - 1
+    $inputArray[$inputEnd..0]
+}
+Get-ChildItem C:\
+Get-ChildItem C:\ | ReverseInput
+
+$Array1 = 1,2,3,4,5
+$array2 = $Array1[4..0]
+$array2
+
+#11.19 Write Pipeline-Oriented Scripts with Cmdlet Keywords
+
+#11.20 Write a Pipeline-Oriented Function
+
+#11.21 Organize Scripts for Improved Readability
+
+#11.22 Invoke Dynamically Named Commands
+
+#11.23 Program:Enhance or Extend an Existing Cmdlet
