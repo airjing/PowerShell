1e8c840eae98ef91a741e273afd89c2888a99dec
Shaojing Chen <airjing@hotmail.com>
2019-01-08 21:03:48 +0800

    run PS script to custom IIS

--- Install_Azure_PowerShell.sh
+++ Install_Azure_PowerShell.sh
@@ -10,3 +10,11 @@ az vm get-instance-view \
   --name myVM \
   --resource-group da46483e-0712-4f63-ac91-bb9062b5406a \
   --output table
+
+az vm extension set \
+  --resource-group e1433d7c-8c66-4099-a1ad-b42db01841d6 \
+  --vm-name myVM \
+  --name CustomScriptExtension \
+  --publisher Microsoft.Compute \
+  --settings '{"fileUris":["https://raw.githubusercontent.com/MicrosoftDocs/mslearn-welcome-to-azure/master/configure-iis.ps1"]}' \
+  --protected-settings '{"commandToExecute": "powershell -ExecutionPolicy Unrestricted -File configure-iis.ps1"}'
