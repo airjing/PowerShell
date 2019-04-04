e24f0a253408fa21ef04db66eee116c1d06359b4
Shaojing Chen <airjing@hotmail.com>
2019-04-04 11:46:54 +0800

    add docker installaton script

--- Post-Configuration-CentOS.sh
+++ Post-Configuration-CentOS.sh
@@ -300,3 +300,7 @@ sudo vi /etc/yum.repos.d/google-chrome.repo
 
 yum info google-chrome-stable
 yum install google-chrome-stable
+#install docker
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
